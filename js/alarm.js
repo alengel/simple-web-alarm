7b7d9ccb2583076506509fd553ffbf381c5a0fa2
Alena Dagneau <alenadagneau@gmail.com>
2016-10-10 20:39:55 -0400

    simple app for Adrian

---
+++ js/alarm.js
@@ -0,0 +1,70 @@
+document.onreadystatechange = function () {
+    if (document.readyState == "complete") {
+        var timer;
+        
+        startTimers(timer);
+        _.delay(setAlarm, 1000);
+   }
+}
+
+function startTimers(timer) {
+    if(timer) {
+        clearInterval(timer);
+    }
+
+    timer = window.setInterval(setTime, 1000);
+
+}
+
+function getCurrentTime() {
+    return moment();
+}
+
+function setTime(now) {
+    var time = document.getElementById('timer');
+    time.textContent = getCurrentTime().format('hh:mm:ss');
+}
+
+function setAlarm() {
+    var randomNumber = pickRandomNumber(),
+        alarm = document.getElementById('alarm'),
+        newAlarm = moment().add(randomNumber, 'seconds');
+    
+    checkAlarm(randomNumber);
+    alarm.textContent = newAlarm.format('hh:mm:ss');
+}
+
+function pickRandomNumber() {
+    return _.random(2, 60);
+}
+
+function checkAlarm(randomNumber) {
+    var alarmTimer = window.setInterval(function() {
+            ringAlarm();
+            resetTimer(alarmTimer);
+        }, randomNumber * 1000);
+}
+
+function ringAlarm() {
+    var audio = new Audio('./audio/bell.mp3');
+    audio.play();
+
+    updateAlarmText();
+}
+
+function resetTimer(alarmTimer) {
+    clearInterval(alarmTimer);
+
+    _.delay(resetAlarmText, 1000);
+    _.delay(setAlarm, 1000);
+}
+
+function updateAlarmText() {
+    document.getElementById('alarm').className = ('alarm red');
+    document.getElementById('alarm-text').textContent = 'Alarm rang at ';
+}
+
+function resetAlarmText () {
+    document.getElementById('alarm').className = ('alarm');
+    document.getElementById('alarm-text').textContent = 'Alarm will ring at ';
+}
